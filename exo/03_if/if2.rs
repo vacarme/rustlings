8a9e289167a98b8f6c142ba2f623bf916aec490d
vacarme <ironex@gmail.com>
2024-02-25 20:09:08 +0100

    03 done

---
+++ exo/03_if/if2.rs
@@ -0,0 +1,30 @@
+pub fn foo_if_fizz(fizzish: &str) -> &str {
+    // Step 1: Make me compile!
+    // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
+    if fizzish == "fizz" {
+        "foo"
+    } else {
+        if fizzish == "fuzz" {"bar"} else {"baz"}
+    }
+}
+
+// No test changes needed!
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn foo_for_fizz() {
+        assert_eq!(foo_if_fizz("fizz"), "foo")
+    }
+
+    #[test]
+    fn bar_for_fuzz() {
+        assert_eq!(foo_if_fizz("fuzz"), "bar")
+    }
+
+    #[test]
+    fn default_to_baz() {
+        assert_eq!(foo_if_fizz("literally anything"), "baz")
+    }
+}
